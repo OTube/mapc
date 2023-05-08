779ae1c8ca45b027beb2c09d81b010b7d1dfbe40
Oleg <69583657+OTube@users.noreply.github.com>
2023-05-08 22:17:13 +0300

    Delete decode.h

--- decode.h
+++
@@ -1,9 +0,0 @@
-#ifndef DECODE_H_INCLUDED
-#define DECODE_H_INCLUDED
-
-#include <stdio.h>
-#include <stdlib.h>
-
-unsigned char *decode(char *from, unsigned int rsize, unsigned int fsize);
-
-#endif // DECODE_H_INCLUDED
